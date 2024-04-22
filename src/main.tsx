7484f01d47e274c6f3ca4f78fa41b9b41d103564
dubin <watire@126.com>
2024-04-22 15:42:45 +0800

    Config github pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'http://192.168.89.101:5080/tonconnect-manifest.json';
+const manifestUrl = 'https://watire.github.io/my-twa/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
